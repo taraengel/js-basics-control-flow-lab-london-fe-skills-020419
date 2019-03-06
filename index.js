98a531b60669241c20ed29e88d60fff76e5e3220
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-06 20:02:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function ternaryCheckCity(city) {
 function switchOnCharmFromTip(tip) {
   switch (tip) {
     case 'generous';
-      return "Thank you so much."
+      return "Thank you so much.";
       break;
   }
 }
