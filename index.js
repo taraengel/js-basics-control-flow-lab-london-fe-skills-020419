806204d7045b003b195edf5aaf434cc34c8c370d
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-06 19:31:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Write your code in this file!
 function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
-    return 'This one is on me!'
-    
-  } else if (feet > 2000 && feet <= 2500) {
-    return 'I will gladly take your thirty bucks.'
-   }
-  else {
-    return 'No can do.'
-}
+    if (feet <= 400) {
+      return 'This one is on me!'
+      
+    } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+     }
+    else {
+      return 'No can do.'
+  }
 
+}
